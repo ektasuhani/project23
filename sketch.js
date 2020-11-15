02a31c58f87612234519255e903cbb5acd82f329
ektasuhani <73230554+ektasuhani@users.noreply.github.com>
2020-11-15 17:52:57 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,74 @@
+var helicopterIMG, helicopterSprite, packageSprite,packageIMG;
+var packageBody,ground
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+function preload()
+{
+	helicopterIMG=loadImage("helicopter.png")
+	packageIMG=loadImage("package.png")
+}
+
+function setup() {
+	createCanvas(800, 700);
+	rectMode(CENTER);
+	
+
+	packageSprite=createSprite(width/2, 80, 10,10);
+	packageSprite.addImage(packageIMG)
+	packageSprite.scale=0.2
+
+	helicopterSprite=createSprite(width/2, 200, 10,10);
+	helicopterSprite.addImage(helicopterIMG)
+	helicopterSprite.scale=0.6
+
+	groundSprite=createSprite(width/2, height-35, width,10);
+	groundSprite.shapeColor=color(255)
+
+    
+	engine = Engine.create();
+	world = engine.world;
+
+	packageBody = Bodies.circle(width/2 , 200 , 5 , {restitution:0.3, isStatic:true});
+	World.add(world, packageBody);
+	
+
+	//Create a Ground
+	ground = Bodies.rectangle(width/2, 650, width, 10 , {isStatic:true} );
+ 	World.add(world, ground);
+    bottomBody=Bodies.rectangle(width/2,640,150,20);
+	leftBody=Bodies.rectangle(width/2-20,640,20,150);
+	rightBody=Bodies.rectangle(width/2+20,640,20,150);
+	Engine.run(engine);
+  World.add(world,bottomBody);
+  World.add(world,leftBody);
+  World.add(world,rightBody);
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(0);
+  packageSprite.x= packageBody.position.x 
+  packageSprite.y= packageBody.position.y 
+  fill ("red");
+  rect(bottomBody.position.x,bottomBody.position.y,150,20)
+  fill ("red");
+  rect(leftBody.position.x,leftBody.position.y,20,150);
+  fill("red")
+  rect(rightBody.position.x,rightBody.position.y,20,150);
+  drawSprites();
+ 
+}
+
+function keyPressed() {
+ if (keyCode === DOWN_ARROW) {
+    // Look at the hints in the document and understand how to make the package body fall only on
+    Matter.Body.setStatic(packageBody,false);
+  }
+}
+
+
+
